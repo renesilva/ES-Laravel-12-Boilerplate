b92053b412db4a8d86a0f76b5d37febf104793bf
ERESSEA RS <rsilva@eresseasolutions.com>
2025-02-25 18:06:52 -0400

    Added Test Controller and .http files

---
+++ app/Http/Controllers/API/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+class TestController extends Controller
+{
+    public function testGet(): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'GET request successful'
+        ]);
+    }
+
+}
+
